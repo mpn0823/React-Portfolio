f91a8990914f34002fcdfe7eda3fd14ef03f51f6
Michael P Nance <michael.p.nance@gmail.com>
2020-08-30 00:39:21 -0400

    Added font awesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 export default () => {
-    return <></>;
+    return <FontAwesomeIcon icon={faCoffee} />;
 };
